3dc9ce9c74db949c572f9fd001e06e1b3cd366c9
ThaYoung1 <davydejong.ddj@gmail.com>
2023-03-04 23:45:32 +0100

    Try out get athlete stats

--- drivers/stravauser/device.js
+++ drivers/stravauser/device.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const Homey = require("homey");
+const StravaAPI = require('strava-v3');
+
+let strava;
+let pollInterval;
+let store;
 
 class StravaUserDevice extends Homey.Device {
   async onInit() {
+    //const settings = this.getSettings();
+    //store = this.getStore();
+
+    //strava = new StravaAPI.client(store.token.access_token);
+
+    //pollInterval = this.homey.setInterval(this.onPoll.bind(this), settings.updateInterval * 1000);
+    //this.onPoll();
   }
-  
+
   /**
    * onAdded is called when the user adds the device, called just after pairing.
    */
@@ -34,6 +46,22 @@ class StravaUserDevice extends Homey.Device {
   async onRenamed(name) {
     this.log('MyDevice was renamed');
     this.log('en dit is de opgeslagen token: ' + JSON.stringify(this.getStoreValue('token')));
+  }
+
+  async onPoll() {
+    let athlete = await strava.athlete.listActivities({ 
+      before: 1677968431,
+      after: 1,
+      page: 1,
+      per_page: 200
+    });
+    let athlete2 = await strava.athlete.listActivities({ 
+      before: 1677968431,
+      after: 1,
+      page: 2,
+      per_page: 200
+    });
+    this.log(JSON.stringify(athlete));
   }
 
 }
