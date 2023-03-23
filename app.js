a55261eb96ff90189e633990a0fd23e0dd4398f1
ThaYoung1 <davydejong.ddj@gmail.com>
2023-03-23 21:39:06 +0100

    Added units to flow tokens and added tempo

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ class StravaApp extends Homey.App {
     
     return hub;
   }
+  
+  toTimeString(totalSeconds) {
+    const totalMs = totalSeconds * 1000;
+    const result = new Date(totalMs).toISOString().slice(11, 19);
+  
+    return result;
+  }
 
   async post(body) {
     this.log('POST called met ' + JSON.stringify(body));
@@ -28,46 +35,69 @@ class StravaApp extends Homey.App {
     if (device){
       // Strava user device trigger detected
       if (body.object_type == 'activity'){
-        let tokens;
+        let tokens = {};
 
         if (body.aspect_type == 'create' || body.aspect_type == 'update') {
           let store = await device.getStoreWithValidToken();
           let strava = new StravaAPI.client(store.token.access_token);
           let activity = await strava.activities.get({id: body.object_id});
-          if (!activity.max_heartrate)
-          activity.max_heartrate = 0
+
+          if (typeof activity.distance !== 'number') {
+            tokens.distance_m = 0;
+            tokens.distance_km = 0;  
+          } else {
+            tokens.distance_m = activity.distance;
+            tokens.distance_km = +(activity.distance / 1000).toFixed(2);
+          }
+
+          if (typeof activity.max_heartrate !== 'number') {
+            tokens.max_heartrate = 0 
+          } else {
+            tokens.max_heartrate = activity.max_heartrate;
+          }
  
-         if (!activity.average_heartrate)
-          activity.average_heartrate = 0
+          if (typeof activity.average_heartrate !== 'number') {
+            tokens.average_heartrate = 0 
+          } else { 
+            tokens.average_heartrate = activity.average_heartrate;
+          }
  
-         if (!activity.average_watts)
-          activity.average_watts = 0
+          if (typeof activity.average_watts !== 'number') {
+            tokens.average_watts = 0
+          } else {
+            tokens.average_watts = activity.average_watts;
+          }
  
-         if (!activity.suffer_score)
-          activity.suffer_score = 0
+          if (typeof activity.suffer_score !== 'number') {
+            tokens.suffer_score = 0
+          } else {
+            tokens.suffer_score = activity.suffer_score;
+          }
  
-         tokens = {
-           id: body.object_id,
-           name: activity.name,
-           description: activity.description,
-           type: activity.type,
-           sport_type: activity.sport_type,
-           start_date_local: activity.start_date_local, 
-           distance: activity.distance,
-           average_speed: activity.average_speed,
-           max_speed: activity.max_speed,
-           total_elevation_gain: activity.total_elevation_gain,
-           average_watts: activity.average_watts,
-           average_heartrate: activity.average_heartrate,
-           max_heartrate: activity.max_heartrate,
-           suffer_score: activity.suffer_score,
-           elapsed_time: activity.elapsed_time,
-           moving_time: activity.moving_time,
-           pr_count: activity.pr_count,
-           commute: activity.commute,
-           private: activity.private,
-           visibility: activity.visibility,
-         };
+          tokens.id = body.object_id;
+          tokens.name = activity.name;
+          tokens.description = activity.description;
+          tokens.type = activity.type;
+          tokens.sport_type = activity.sport_type;
+          tokens.start_date_local = activity.start_date_local;
+
+          tokens.average_speed_ms = activity.average_speed;
+          tokens.average_speed_kph = +(activity.average_speed * 3.6).toFixed(2);
+          tokens.average_tempo_minkm = new Date(((1 / activity.average_speed) * 1000) * 1000).toISOString().substring(14, 19);
+          
+          tokens.max_speed = activity.max_speed;
+          tokens.total_elevation_gain = activity.total_elevation_gain;
+
+          tokens.elapsed_time_s = activity.elapsed_time;
+          tokens.elapsed_time_hh_mm_ss = this.toTimeString(activity.elapsed_time);
+
+          tokens.moving_time_s = activity.moving_time;
+          tokens.moving_time_hh_mm_ss = this.toTimeString(activity.moving_time);
+
+          tokens.pr_count = activity.pr_count;
+          tokens.commute = activity.commute;
+          tokens.private = activity.private;
+          tokens.visibility = activity.visibility;
         }
 
         switch (body.aspect_type){
